6b7d27f54d13cdb61a3881006cf4f45385ab3e72
Victor Hugo <victorhsmsantos@gmail.com>
2022-01-06 20:23:57 -0300

    requisito 9 feito, falta melhorar sintaxe

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,17 @@
+const { employees } = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
+// passado o id de um funcionário, encontra a primeira espécie de animal gerenciado pelo funcionário, e retorna um array com nome, sexo e idade do animal mais velho dessa espécie.
 
+const teste = (acc, animal) => Math.max(acc, animal.age);
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const funcionarioEncontrado = employees.find((funcionario) => funcionario.id === id);
+  const animaisResp = funcionarioEncontrado.responsibleFor[0];
+  const animalEncontrado = species.find((animal) => animal.id === animaisResp);
+  const mIdade = animalEncontrado.residents.reduce(teste, 0);
+  const animalFinal = animalEncontrado.residents.find((animal) => animal.age === mIdade);
+  const resultado = [animalFinal.name, animalFinal.sex, animalFinal.age];
+  return resultado;
 }
 
 module.exports = getOldestFromFirstSpecies;
