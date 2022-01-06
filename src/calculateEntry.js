ea38911d7d77b718165bba4ee71c6ed2a6d0ab17
Victor Hugo <victorhsmsantos@gmail.com>
2022-01-06 18:48:57 -0300

    requisito 6 quase completo

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,22 @@
+const { prices } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function countEntrants(entrants) {
-  // seu código aqui
+const child = (person) => person.age < 18;
+const adult = (person) => person.age >= 18 && person.age < 50;
+const senior = (person) => person.age >= 50;
+
+function countEntrants(entrants = {}) {
+  return entrants === {} ? 0 : {
+    child: entrants.filter(child).length,
+    adult: entrants.filter(adult).length,
+    senior: entrants.filter(senior).length,
+  };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  const countPersons = countEntrants(entrants);
+  const keys = Object.keys(prices);
+  return parseFloat(keys.reduce((acc, key) => acc + prices[key] * countPersons[key], 0));
 }
 
 module.exports = { calculateEntry, countEntrants };
