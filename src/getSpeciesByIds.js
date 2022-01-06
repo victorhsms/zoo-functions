8bb5172c0f99d04a4989a36d44a4da5f3d1c3dcb
Victor Hugo <victorhsmsantos@gmail.com>
2022-01-06 15:26:58 -0300

    projeto iniciado. Primeiro requisito feito! :rocket:

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,9 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  // const result = species.map((specie) => ids.find((id) => specie.id === id));
+  return ids.map((id) => species.find((specie) => specie.id === id));
 }
 
 module.exports = getSpeciesByIds;
