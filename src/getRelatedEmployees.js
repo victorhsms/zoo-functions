8716a5905a1515465e858adeb12ffd42fd13c8bc
Victor Hugo <victorhsmsantos@gmail.com>
2022-01-07 11:32:32 -0300

    Requisito 4 feito! :rocket:

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,20 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function isManager(id) {
-  // seu código aqui
+const mkArray = (item) => `${item.firstName} ${item.lastName}`;
+// ref: https://www.horadecodar.com.br/2020/01/23/javascript-verificar-se-um-elemento-existe-no-array/
+const searchEmployees = (id) => employees.filter((e) => e.managers.includes(id)).map(mkArray);
+
+function isManager(id) { // Verifica em cada funcionario se ele tem como gerente alguém com ID passado como parâmetro.
+  return employees.some((person) => person.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (!(isManager(managerId))) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  } else {
+    return searchEmployees(managerId);
+  }
 }
 
 module.exports = { isManager, getRelatedEmployees };
